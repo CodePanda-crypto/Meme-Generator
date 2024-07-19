b2c7be45dca10a16b0314b4edb4280834727bb4f
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-19 16:29:52 +0500

    html and styling for the man component

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -6,5 +6,27 @@ import './main.css';
 export default function Main() {
   // const cards = CardData.map((card) => <Card key={card.id} {...card} />);
 
-  return <main className="main-container"></main>;
+  return (
+    <main className="main-container">
+      <div className="input-container">
+        <div className="input-wrapper">
+          <label htmlFor="top-text">Top Text</label>
+          <input type="text" id="top-text" placeholder="Enter Your Text" />
+          <label htmlFor="bottom-text">Bottom Text</label>
+          <input type="text" id="bottom-text" placeholder="Enter Your Text" />
+        </div>
+        <button type="button" className="content-button">
+          Get a new meme image
+          <img
+            src="/images/magic-wand.png"
+            alt="magic wand"
+            className="magic-wand"
+          />
+        </button>
+      </div>
+      <div className="meme-container">
+        <img src="/images/group.png" alt="meme" />
+      </div>
+    </main>
+  );
 }
